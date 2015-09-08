d36c97d7758db305a1b6fa9a4f45428c464a4131
Kenan Mahmutovic <kmahmutovi1@etf.unsa.ba>
2015-09-08 14:44:39 +0200

    3. Spirala
    3. Spirala

--- validateForm.js
+++ validateForm.js
@@ -1,8 +1,13 @@
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 function validateForm() {
     var ime = document.kontaktforma.ime;
     var prezime = document.kontaktforma.prezime;
     var email = document.kontaktforma.email;
     var poruka = document.kontaktforma.poruka;
+    var broj = document.kontaktforma.broj;
     var pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/i;
     if (ime.value == null || ime.value == "") {
 
@@ -34,6 +39,23 @@ function validateForm() {
     }
     else {
         document.getElementById('emailError').style.visibility = 'hidden';
+    }
+
+    if(broj.value != "")
+    {
+        if(!isNumeric(broj.value))
+        {
+        document.getElementById('brojError').style.visibility = 'visible';
+        return false;
+        }
+        else
+        {
+            document.getElementById('brojError').style.visibility = 'hidden';
+        }
+    }
+    else
+    {
+        document.getElementById('brojError').style.visibility = 'hidden';
     }
 
     if (poruka.value == "") {
